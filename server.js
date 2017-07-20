d3ea22863077f9b57279c56df86905d7f07624f9
TannerGruebele <tanner@saambaa.com>
2017-07-20 00:57:40 -0700

    setup files / front and back end

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let path = require('path');
+let bodyParser = require("body-parser");
+
+let app = express();
+
+app.use('/assets', express.static('assets'));
+
+let PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});

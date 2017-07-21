f0077f91df812c924386e2bc6dd042021470689f
TannerGruebele <tanner@saambaa.com>
2017-07-21 15:51:17 -0700

    Finished matching logic, added more dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,15 +2,75 @@ var friends = [{
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
-        "5",
+        "3",
         "1",
-        "4",
+        "2",
         "4",
         "5",
-        "1",
+        "2",
         "2",
         "5",
         "4",
+        "5"
+    ]
+}, {
+    "name": "Greggory",
+    "photo": "",
+    "scores": [
+        "5",
+        "5",
+        "4",
+        "4",
+        "2",
+        "2",
+        "2",
+        "5",
+        "3",
+        "5"
+    ]
+}, {
+    "name": "Harry",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiOreWZ65nVAhVLJCYKHVI4B5AQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Fwinter%2F&psig=AFQjCNEMvIUj8tT6XDsXmbJVRbG3WoPuwA&ust=1500707994175769",
+    "scores": [
+        "2",
+        "1",
+        "5",
+        "3",
+        "5",
+        "1",
+        "2",
+        "2",
+        "4",
+        "5"
+    ]
+}, {
+    "name": "George",
+    "photo": "https://www.biography.com/.image/t_share/MTIwNjA4NjM0MDA1MzkwODYw/george-washington-9524786-1-402.jpg",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+    ]
+}, {
+    "name": "Kip",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj-9s6265nVAhVI5SYKHX4TB50QjRwIBw&url=https%3A%2F%2Fwww.planwallpaper.com%2Fimages&psig=AFQjCNEMvIUj8tT6XDsXmbJVRbG3WoPuwA&ust=1500707994175769",
+    "scores": [
+        "1",
+        "5",
+        "4",
+        "5",
+        "2",
+        "4",
+        "2",
+        "5",
+        "5",
         "1"
     ]
 }]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,49 @@
 let friends = require('../data/friends.js');
 
 module.exports = function(app) {
+    // Gets api data for api display
     app.get('/api/friends', function(req, res) {
         res.json(friends)
     })
 
     app.post('/api/friends', function(req, res) {
+        let userName = req.body.name;
+        let userScores = req.body.scores;
+        let currentLow = 100;
+        let match = {};
+
+        friends.forEach(function(friend) {
+            let newDifference = matchCheck(userScores, friend.scores)
+            if (newDifference < currentLow) {
+                currentLow = newDifference;
+                match = {
+                    name: friend.name,
+                    photo: friend.photo
+                }
+            }
+
+        }, this);
+
+        console.log('------------------------------------');
+        console.log(match.name);
+        console.log('------------------------------------');
+        res.status(200).send(match);
         friends.push(req.body);
     })
 };
+
+function matchCheck(userArray, friendArray) {
+    let redArray = [];
+    let newDifference = 0;
+
+    for (var i = 0; i < userArray.length; i++) {
+        redArray.push(Math.abs(userArray[i] = friendArray[i]))
+    }
+
+    newDifference = redArray.reduce(function(a, b) {
+        return a + b;
+    })
+
+    return newDifference;
+
+}

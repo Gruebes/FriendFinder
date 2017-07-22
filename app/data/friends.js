a60b403345c872cdb0dc304ec1a29a25b0875b5e
TannerGruebele <tanner@saambaa.com>
2017-07-22 11:20:20 -0700

    Fixed faker

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,83 +1,3 @@
-var friends = [{
-    "name": "Abraham",
-    "photo": "http://diversifiedantiques.com/images/listings/446_large.jpg",
-    "scores": [
-        "3",
-        "1",
-        "2",
-        "4",
-        "5",
-        "2",
-        "2",
-        "5",
-        "4",
-        "5"
-    ]
-}, {
-    "name": "John",
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/5/5e/John_F._Kennedy%2C_White_House_photo_portrait%2C_looking_up.jpg",
-    "scores": [
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5"
-    ]
-}, {
-    "name": "Robert",
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/b/bd/Robert_F_Kennedy_crop.jpg",
-    "scores": [
-        "2",
-        "1",
-        "5",
-        "3",
-        "5",
-        "1",
-        "2",
-        "2",
-        "4",
-        "5"
-    ]
-}, {
-    "name": "George",
-    "photo": "https://www.biography.com/.image/t_share/MTIwNjA4NjM0MDA1MzkwODYw/george-washington-9524786-1-402.jpg",
-    "scores": [
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1"
-    ]
-}, {
-    "name": "Dick",
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Richard_M._Nixon%2C_ca._1935_-_1982_-_NARA_-_530679.jpg/220px-Richard_M._Nixon%2C_ca._1935_-_1982_-_NARA_-_530679.jpg",
-    "scores": [
-        "1",
-        "5",
-        "4",
-        "5",
-        "2",
-        "4",
-        "2",
-        "2",
-        "5",
-        "1"
-    ]
-}]
-
-
-module.exports = friends;
-
 var faker = require('faker');
 
 var fakeFriends = []
@@ -85,18 +5,14 @@ var fakeFriends = []
 for (var i = 0; i < 50; i++) {
 
     fakeFriend = {
-        randomName: faker.name.findName(),
-        randomAvatar: faker.image.avatar(),
-        randomAnswers: []
+        name: faker.name.findName(),
+        photo: faker.image.avatar(),
+        scores: []
     };
     for (var a = 0; a < 10; a++) {
-        fakeFriend.randomAnswers.push(Math.floor(Math.random() * 5) + 1);
+        fakeFriend.scores.push(Math.floor(Math.random() * 5) + 1);
     }
     fakeFriends.push(fakeFriend)
 }
 
-
-// console.log('------------------------------------');
-// console.log(fakeFriends);
-
-// console.log('------------------------------------');
+module.exports = fakeFriends;
